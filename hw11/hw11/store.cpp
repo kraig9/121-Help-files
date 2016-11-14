03cd82d500197ecd640ffd2a464c44ed3ea05891
kwo227 <kwo227@tamu.edu>
2016-11-13 22:39:41 -0600

    temporary changes

--- hw11/hw11/store.cpp
+++ hw11/hw11/store.cpp
@@ -17,6 +17,31 @@ void Store::addCustomer(int customerID, string customerName) {
 	Customer temp_cust(customerID, customerName, true);
 	Store::customers.push_back(temp_cust);
 }
+
+//hw11
+Product& Store::getProduct(int productID) {
+	int*  it;
+	it=std::find(products.begin(), products.end(), productID);
+}
+Customer& Store::getCustomer(int customerID) {
+
+}
+void Store::takeShipment(int productID, int quantity, double cost) {
+
+}
+void Store::makePurchase(int customerID, int productID, int quantity) {
+
+}
+void Store::takePayment(int customerID, double amount) {
+
+}
+void Store::listProducts() {
+
+}
+void Store::listCustomers() {
+
+}
+
 catch (1) {
 	cout << "bad ID";
 }
--- hw11/hw11/store.h
+++ hw11/hw11/store.h
@@ -17,5 +17,15 @@ public:
 	void setName(string name);
 	void addProduct(int productID, string productName);
 	void addCustomer(int customerID, string customerName);
+
+	//hw11
+	Product& getProduct(int productID);
+	Customer& getCustomer(int customerID);
+	void takeShipment(int productID, int quantity, double cost);
+	void makePurchase(int customerID, int productID, int quantity);
+	void takePayment(int customerID, double amount);
+	void listProducts();
+	void listCustomers();
+
 };
 #endif
